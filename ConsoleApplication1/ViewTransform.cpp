c28f7d40f5175afc75af8390b8d8a619e0e8a361
user <rhwkdel1@naver.com>
2022-10-06 10:32:49 +0900

    시점변환

---
+++ ConsoleApplication1/ViewTransform.cpp
@@ -0,0 +1,67 @@
+#include<GL/glut.h>
+#include<stdio.h>
+
+void MyDisplay();
+void MyReshape(int w, int h);
+
+void InitLight() {
+	GLfloat mat_diffuse[] = { 0.5, 0.4, 0.3, 1.0 };
+	GLfloat mat_specular[] = { 1.0, 1.0, 1.0,1.0 };
+	GLfloat mat_ambient[] = { 0.5, 0.4, 0.3, 1.0 };
+	GLfloat mat_shininess[] = { 15.0 };
+	GLfloat light_specular[] = { 1.0, 1.0, 1.0, 1.0 };
+	GLfloat light_diffuse[] = { 0.8, 0.8, 0.8, 1.0 };
+	GLfloat light_ambient[] = { 0.3, 0.3, 0.3, 1.0 };
+	GLfloat light_position[] = { -3, 6, 3.0, 0.0 };
+	glShadeModel(GL_SMOOTH);
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+	glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
+	glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
+	glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
+	glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse);
+	glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
+	glMaterialfv(GL_FRONT, GL_AMBIENT, mat_ambient);
+	glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
+}
+
+int main(int argc, char** argv) {
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGBA);
+	glutInitWindowSize(500, 500);
+	glutCreateWindow("박재민");
+	glClearColor(0.4, 0.4, 0.4, 0.0);
+	InitLight();
+	glutDisplayFunc(MyDisplay);
+	glutReshapeFunc(MyReshape);
+	glutMainLoop();
+	return 0;
+}
+
+void MyDisplay() {
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	//gluLookAt(0.0, 0.0, 0.0, 0.0, 0.0, -1.0, 0.0, 1.0, 0.0); //원점, 초점, 기울임(상향벡터)
+	gluLookAt(-0.6, 0.0, 0.0, 0.0, 0.0, -1.0, 0.0, 1.0, 0.0);
+	//gluLookAt(0.6, 0.0, 0.0, 0.0, 0.0, -1.0, 0.0, 1.0, 0.0);
+	
+
+	//gluLookAt(0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0, 0.0); // 초점 (물체의 좌측에서 우로 view)
+	//gluLookAt(0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0); // 초점 (물체의 아래에서 위로 view)
+
+	//gluLookAt(0.0, 0.0, 0.0, 0.0, 0.0, -1.0, 1.0, 0.0, 0.0); // 카메라를 시계방향으로 90도 회전
+	//gluLookAt(0.0, 0.0, 0.0, 0.0, 0.0, -1.0, 1.0, 1.0, 0.0); // 카메라를 시계방향으로 45도 회전
+
+	glutSolidTeapot(0.5);
+	glFlush();
+}
+
+void MyReshape(int w, int h) {
+	glViewport(0, 0, (GLsizei)w, (GLsizei)h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
+}
